76ea8a8173e9a0a38b23e47671574ba0063d1a91
nasum <tomato.wonder.life@gmail.com>
2021-02-01 01:05:01 +0900

    2021/02/01 01:04:00

---
+++ 25_exercise_loops_and_functions.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	tmp := 0.0
+	for i := 0; i < 10; i++ {
+		z = z - (z*z-x)/(2*z)
+		if math.Abs(z-tmp) < 1e-10 {
+			break
+		}
+		tmp = z
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
